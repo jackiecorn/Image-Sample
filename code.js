e0b0268041553e572021cf955678316c238d6667
Jackie Chui <jackie2009hk@gmail.com>
2020-04-11 11:54:29 -0700

    Initial commit

---
+++ code.js
@@ -0,0 +1,18 @@
+if (figma.currentPage.selection.length > 0) {
+    const selection = figma.currentPage.selection[0];
+    selection
+        .exportAsync({
+        format: 'PNG',
+        constraint: { type: 'SCALE', value: 2 }
+    })
+        .then(imageBuffer => {
+        figma.showUI(__html__, { width: selection.width, height: selection.height });
+        figma.ui.postMessage({
+            image: imageBuffer,
+            width: selection.width,
+            height: selection.height
+        });
+    });
+}
+else
+    figma.closePlugin('Please select a layer.');
